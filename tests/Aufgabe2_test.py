60bbda48b38efdfc3374be6b8f169c5d710abf2a
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-03-08 19:37:36 +0100

    add test for every issue

--- tests/Aufgabe2_test.py
+++ tests/Aufgabe2_test.py
@@ -5,21 +5,27 @@ from os.path import dirname
 cwd = dirname(os.getcwd())
 
 directory = './index.md'
-search = str(sys.argv[1])
+
 
 def check_if_string_in_file(file_name, string_to_search):
-	count = 0
+	heading = False
 	f = open(file_name, 'r')
 	Lines = f.readlines() 
 	regEx = string_to_search + '([A-Z]|[a-z])+'
 	for line in Lines:
-		if string_to_search in line:
+		if re.match(regEx, line):
+			heading = True
+			continue
+		if re.match('## ([A-Z]|[a-z])+', line):
+			heading = False
+			continue
+		if (heading == True) and re.findall('([A-Z]|[a-z])+', line):
 			return True
 	f.close()
 	return False
 
 
-if check_if_string_in_file(directory, (search + ' ')):
+if check_if_string_in_file(directory, '# '):
    print('Yes')
 else:
    print('No')
